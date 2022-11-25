f085564137fc9c9e54360586ae60d47aa72202dc
3top1a <3top1a.official@gmail.com>
2022-11-25 22:45:19 +0100

    Transition to view instead of modal

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,47 @@ import type { TimelinesSettings } from './types';
 import { DEFAULT_SETTINGS, TIMELINE_ICON } from './constants';
 import { TimelinesSettingTab } from './settings';
 import { Plugin, addIcon } from 'obsidian';
-import { TimelineMacro } from './modal';
+import { TimelineView, VIEW_TYPE_TIMELINE } from "./view";
 
 export default class TimelinesPlugin extends Plugin {
 	settings: TimelinesSettings;
 
 	async onload() {
 		// Load message
-		await this.loadSettings();
 		console.log('Loaded History Timelines Plugin');
+
+		await this.loadSettings();
 
 		// Register timeline icon and side button
 		addIcon("timeline-timeline", TIMELINE_ICON);
 		this.addRibbonIcon("timeline-timeline", "Timeline", async () => {
-			new TimelineMacro(this.app, this).open();
+			this.activateView();
 		});
+
+		this.registerView(
+			VIEW_TYPE_TIMELINE,
+			(leaf) => new TimelineView(leaf, this)
+		);
 
 		this.addSettingTab(new TimelinesSettingTab(this.app, this));
 	}
 
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_TIMELINE);
+
+		await this.app.workspace.getLeaf(false).setViewState({
+			type: VIEW_TYPE_TIMELINE,
+			active: true,
+		});
+
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_TIMELINE)[0]
+		);
+	}
+
 	onunload() {
 		console.log('Unloading History Timelines plugin');
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_TIMELINE);
 	}
 
 	async loadSettings() {
